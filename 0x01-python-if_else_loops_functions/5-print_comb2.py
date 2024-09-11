dfbe9fadf32af8e9c959ef4707c16c787701e9fc
Amir <amirsadik636@gmail.com>
2024-09-11 19:30:46 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    print("{}, ".format(str(i).rjust(2, '0')), end='')
-print('')
+    if (i < 99):
+        print("{}, ".format(str(i).rjust(2, '0')), end='')
+    else:
+        print(i)
