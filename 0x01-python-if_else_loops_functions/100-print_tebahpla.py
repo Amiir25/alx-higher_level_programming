2fca53c118af89c70d188d3d6b628474739d7c4b
Amir <amirsadik636@gmail.com>
2024-09-11 23:55:29 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-counter = 1
 for i in reversed(range(ord('a'), ord('z') + 1)):
-    if (counter % 2 == 0):
+    if (i % 2 != 0):
         i = i - 32
 
-    counter += 1
     print(chr(i), end='')
