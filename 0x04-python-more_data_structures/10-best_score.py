a146cd89f28953dd136c8fe804b49123899457e4
Amiir25 <amirsadik636@gmail.com>
2024-09-19 04:58:36 +0300

    Add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best = 0
+    for key, value in a_dictionary.items():
+        if value > best:
+            best = value
+
+    return best
