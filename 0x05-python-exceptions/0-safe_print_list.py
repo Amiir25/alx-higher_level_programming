4dfeebb847c4c3970482f996b1142ceee8c6e4c7
Amiir25 <amirsadik636@gmail.com>
2024-09-30 12:39:30 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0;
+    count = 0
 
     try:
         for i in my_list:
             if count == x:
-                break;
+                break
 
             if i >= 0 or i <= 9:
                 print("{:d}".format(i), end='')
                 count += 1
         print("")
 
-    except:
-        print("Unknown Error");
+    except IndexError:
+        print("Unknown Error")
 
     return count
