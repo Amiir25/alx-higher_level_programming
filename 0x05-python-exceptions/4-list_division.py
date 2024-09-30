9076d9620b4a96fb0aaebab36b09788ef0c17969
Amiir25 <amirsadik636@gmail.com>
2024-09-30 17:21:03 +0300

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
         except ZeroDivisionError:
             result = 0
-            print("division by zero")
+            print("division by 0")
 
         except IndexError:
             result = 0
