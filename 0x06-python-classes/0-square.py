f92230987c1dbd25c8c0e896bd9da78a6e0c6070
Amiir25 <amirsadik636@gmail.com>
2024-10-01 17:57:15 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
+"""
+0-square.py
+
+This module provides an empty class called Square
+that defines a square
+"""
+
+
 class Square:
+    """
+    The Square class is an empty that defines a square
+    """
     pass
