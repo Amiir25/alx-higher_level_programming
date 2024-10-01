33ac3bdbaec11cfd59b92295def4de9eb07475ac
Amiir25 <amirsadik636@gmail.com>
2024-10-01 21:48:44 +0300

    Add 6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+"""
+0-square.py
+
+This module provides a class called Square that defines
+a square by private instance attribute 'size'
+"""
+
+
+class Square:
+    """
+    The Square class defines a square by private instance
+    attribute 'size'
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if isinstance(position, tuple):
+            if all(isinstance(i, int) and i > 0 for i in position):
+                self.__position = position
+
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """
+        Calculates area
+
+        Returns:
+            The area of a square
+        """
+
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Prints a square of # character
+
+        Returns:
+            Nothing
+        """
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                print(" ", sep='', end='')
+
+            for j in range(self.__size):
+                print("#", sep='', end='')
+            print()
