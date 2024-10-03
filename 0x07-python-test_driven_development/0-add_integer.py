0afa780e222d35896df83a01f6933f3aa9dc562f
Amiir25 <amirsadik636@gmail.com>
2024-10-03 17:33:08 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    This module provides a function that adds two numbers.
+    The numbers can be either integers or floats. Floats 
+    can be coverted into integers.
+"""
 
 def add_integer(a, b=98):
 
