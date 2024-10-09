28bb2a44fdc17acb8bafb4c8752420018581911e
Amiir25 <amirsadik636@gmail.com>
2024-10-09 10:25:08 +0300

    Update 3-say_my_name.py tests/3-say_my_name.txt

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,6 +18,9 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
+    if first_name == "" and last_name == "":
+        raise TypeError("empty string")
+
     print("My name is {} {}".format(first_name, last_name))
 
 
