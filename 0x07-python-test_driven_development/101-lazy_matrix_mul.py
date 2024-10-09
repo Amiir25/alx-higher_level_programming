15cf6ec117ff21feb7c981640b3b7a9beb951e54
Amiir25 <amirsadik636@gmail.com>
2024-10-09 19:20:38 +0300

    Add 101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+101-lazy_matrix_mul.py
+
+To multiply matrices using Numpy
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """A function to multiply matrices using Numpy"""
+
+    return np.dot(m_a, m_b)
