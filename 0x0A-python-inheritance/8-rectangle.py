a152ff085d55dae0033d962ee9a02974688c67aa
Amiir25 <amirsadik636@gmail.com>
2024-10-15 02:03:30 +0300

    Add 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+8-rectangle.py
+
+This module provides a class 'Rectangle' that inherits
+from class 'BaseGeometry' and instatiates private attributes
+'width' and 'height'.
+"""
+
+
+class BaseGeometry:
+    """Contains a method that rasies an exception"""
+
+    def area(self):
+        """Raises an exception when called"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """Inherits from 'BaseGeometry' and instantiates private attributes"""
+
+    def __init__(self, width, height):
+        """Instantiates width and height"""
+
+        self.integer_validator("width", height)
+        self.integer_validator("height", width)
+
+        self.__width = width
+        self.__height = height
