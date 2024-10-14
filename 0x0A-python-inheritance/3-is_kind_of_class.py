8d34c247f5cf5430149fedce31191e115659b9e6
Amiir25 <amirsadik636@gmail.com>
2024-10-14 19:05:32 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,11 +4,12 @@
 2-is_kind_of_class.py
 
 This module provides a function that checks if an
-object is an instance a paret or child class.
+object is an instance a specified class or an instance
+of any class that inherits from the specified class.
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """Checks if an object is an instance of a class"""
+    """Checks if an object is an instance of a class or its child classes"""
 
     return isinstance(obj, a_class)
