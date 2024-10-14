070de3a3a5a567a1fa12eceb8fbe9a52719bd9bb
Amiir25 <amirsadik636@gmail.com>
2024-10-14 21:29:35 +0300

    Add 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+7-base_geometry.py
+
+This module provides a class with a method that
+raises an exception and a method that validates
+a value.
+"""
+
+
+class BaseGeometry:
+    """Contains a method that rasies an exception"""
+
+    def area(self):
+        """Raises an exception when called"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
