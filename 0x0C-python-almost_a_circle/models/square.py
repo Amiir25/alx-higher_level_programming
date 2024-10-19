9fee0a5c7e98198f77cfca8de5d84383ab992a7f
Amiir25 <amirsadik636@gmail.com>
2024-10-19 17:18:36 +0300

    Add models/square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+square.py
+
+This module provides a 'Square' class that iherits the
+'Rectangle' class of 'rectangle.py' module.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Inherits the 'Rectangle' class of 'rectangle.py'"""
