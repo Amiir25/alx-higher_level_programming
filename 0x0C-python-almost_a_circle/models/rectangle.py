e61b05ee8ee0037d9530aaf99fce8e1c19fb39a5
Amiir25 <amirsadik636@gmail.com>
2024-10-19 13:55:41 +0300

    Update models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,3 +120,21 @@ class Rectangle(Base):
         for _ in range(self.__height):
             print(" " * self.__x, end='')
             print(row)
+
+    def update(self, *args):
+        """Assignes an srgument to each private instance attribute"""
+
+        if len(args) > 0:
+            self.__id = args[0]
+
+        if len(args) > 1:
+            self.__width = args[1]
+
+        if len(args) > 2:
+            self.__height = args[2]
+
+        if len(args) > 3:
+            self.__x = args[3]
+
+        if len(args) > 4:
+            self.__y = args[4]
