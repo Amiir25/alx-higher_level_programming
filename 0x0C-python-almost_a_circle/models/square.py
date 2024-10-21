a4e6321110f66d1cbbdd02d797cc53bc2ddf9118
Amiir25 <amirsadik636@gmail.com>
2024-10-21 20:53:35 +0300

    Update models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,13 +13,29 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Inherits the 'Rectangle' class of 'rectangle.py'"""
 
-    def __init__(slef, size, x=0, y=0, id=None):
-        """ """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Calls the constructor of the Rectangle class and the
+        width and height values are set to 'size'.
+        """
 
-        super().__init__(width=size, height=size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ """
+        """Overrides the __str__ method to return a formatted output"""
 
         return ("[Square] ({}) {}/{} - {}".
                 format(self.id, self.x, self.y, self.height))
+
+    @property
+    def size(self):
+        """Getter for size, which is the same as width """
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size, which updates both width and height"""
+
+        self.width = value
+        self.height = value
