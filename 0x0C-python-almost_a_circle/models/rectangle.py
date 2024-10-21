3da3f60faec05373c08decfd480e0a7e9392de7e
Amiir25 <amirsadik636@gmail.com>
2024-10-21 19:11:33 +0300

    Update models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,6 +116,9 @@ class Rectangle(Base):
     def display(self):
         """Prints the 'Rectangle' instance with the character #"""
 
+        for _ in range(self.y):
+            print()
+
         row = "#" * self.width
         for _ in range(self.height):
             print(" " * self.x, end='')
