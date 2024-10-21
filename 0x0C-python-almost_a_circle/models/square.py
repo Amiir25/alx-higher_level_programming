d8a2e0e200fc1d716996b2fc8a9065c12f6761e6
Amiir25 <amirsadik636@gmail.com>
2024-10-21 07:38:04 +0300

    Update models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,14 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Inherits the 'Rectangle' class of 'rectangle.py'"""
+
+    def __init__(slef, size, x=0, y=0, id=None):
+        """ """
+
+        super().__init__(width=size, height=size, x, y, id)
+
+    def __str__(self):
+        """ """
+
+        return ("[Square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y, self.height))
