116435772095ec706381f813a6437912a4789c0b
Amiir25 <amirsadik636@gmail.com>
2024-11-19 22:05:59 +0300

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,14 @@
 -- A SQL query to create a new table and insert
 -- records
+
+-- Create second_table if it's not exist
 CREATE TABLE IF NOT EXIST second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
+
+-- Inserts rows into second_table
 INSERT INTO second_table (id, name, score) VALUES
 (1, "John", 10),
 (2, "Alex", 3),
