5df53fd8184abab60c80ec21de13d86f363414bb
Amiir25 <amirsadik636@gmail.com>
2024-11-20 17:27:46 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,7 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd'
+IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Give select permission
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
