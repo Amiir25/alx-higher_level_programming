7f8b94f8ad1236232c370faa80974f52511eb665
Amiir25 <amirsadik636@gmail.com>
2024-11-20 20:06:11 +0300

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- This SQL query displays the title of a tv show with
 -- the id of its genre and NULL if it has no genre.
 SELECT tv_shows.title,
-IFNULL(tv_show_genres.genre_id, NULL) AS genre_id
+COALESCE(tv_show_genres.genre_id, NULL) AS genre_id
 FROM tv_shows
 JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
