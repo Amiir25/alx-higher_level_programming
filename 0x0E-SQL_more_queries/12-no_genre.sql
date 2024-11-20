73fd25030bfc6cb9e5060f489dfc25505b742a46
Amiir25 <amirsadik636@gmail.com>
2024-11-20 20:09:33 +0300

    Add 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- This SQL query displays the title of a tv show with
+-- the id of its genre.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
