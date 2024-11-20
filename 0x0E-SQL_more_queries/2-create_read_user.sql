8ab91d1ef92e2ad9e2f8435f7386e43938c866c2
Amiir25 <amirsadik636@gmail.com>
2024-11-20 17:22:23 +0300

    Add 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- A SQL query that creates a datatbase and a user and gives
+-- the user select privilege.
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd'
+
+-- Give select permission
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost'
