42bbb133a9c07a506127f3b1e5be186b085e27f5
Amiir25 <amirsadik636@gmail.com>
2024-11-20 20:03:11 +0300

    Add 11-genre_id_all_show.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_show.sql
@@ -0,0 +1,8 @@
+-- This SQL query displays the title of a tv show with
+-- the id of its genre and NULL if it has no genre.
+SELECT tv_shows.title,
+IFNULL(tv_show_genres.genre_id, NULL) AS genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
