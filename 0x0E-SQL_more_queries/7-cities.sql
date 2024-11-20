5af49028d65164d5a5b2ff4b25c9d04700dedcee
Amiir25 <amirsadik636@gmail.com>
2024-11-20 17:58:35 +0300

    Add 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,21 @@
+-- This SQL query creates a database and a table
+-- in the database
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the table
+CREATE TABLE IF NOT EXISTS cities(
+    id
+       INT
+       UNIQUE
+       AUTO_INCREMENT
+       NOT NULL
+       PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
