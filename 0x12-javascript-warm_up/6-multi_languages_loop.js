0e3fb2d46696a54cb881de8a92107bdcacf32886
Amiir25 <amirsadik636@gmail.com>
2024-12-19 19:03:19 +0300

    Add 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arr = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let count = 0; count < arr.length; count++) {
+  console.log(arr[count]);
+}
