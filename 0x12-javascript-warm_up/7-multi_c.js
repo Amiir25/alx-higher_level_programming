632b5719b6497ce3ea6207a6b44623dfdad8e312
Amiir25 <amirsadik636@gmail.com>
2024-12-19 19:09:10 +0300

    Add 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const num = process.argv[2];
+if (num) {
+  for (let count = 1; count <= num; count++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occureneces');
+}
