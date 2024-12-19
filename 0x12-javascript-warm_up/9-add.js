2ecad749ead6dd9a9ac64647a2c2d32d754ae80b
Amiir25 <amirsadik636@gmail.com>
2024-12-19 20:14:16 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,4 +6,9 @@ function add (a, b) {
 
 const a = parseInt(process.argv[2], 10);
 const b = parseInt(process.argv[3], 10);
-console.log(add(a, b));
+
+if (isNaN(a) || isNaN(b)) {
+  console.log(NaN);
+} else {
+  console.log(add(a, b));
+}
