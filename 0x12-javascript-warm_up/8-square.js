4d0894c130d23033b47388831c11b7ffcfd15e8b
Amiir25 <amirsadik636@gmail.com>
2024-12-19 19:20:18 +0300

    Add 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (!isNaN(num)) {
+  for (let i = 1; i <= num; i++) {
+    let x = '';
+    for (let j = 1; j <= num; j++) {
+      x += 'X';
+    }
+    console.log(x);
+  }
+}
