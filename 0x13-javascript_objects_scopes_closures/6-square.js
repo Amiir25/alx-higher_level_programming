6055e9988cde617dde54de3807018df379d35364
Amiir25 <amirsadik636@gmail.com>
2024-12-24 14:24:33 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,14 @@ const parentSquare = require('./5-square');
 
 class Square extends parentSquare {
   constructor (square) {
-    super();
-    this.square = square;
+    super(square, square);
   }
 
   charPrint (c) {
     const exec = c || 'X';
-    for (let i = 0; i < this.square; i++) {
+    for (let i = 0; i < this.height; i++) {
       let temp = '';
-      for (let j = 0; j < this.square; j++) {
+      for (let j = 0; j < this.height; j++) {
         temp += exec;
       }
       console.log(temp);
