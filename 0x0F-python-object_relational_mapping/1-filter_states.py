cb3899c5bdac480a297c02a702c1f4dcccedca1d
Amiir25 <amirsadik636@gmail.com>
2024-12-29 16:51:51 +0300

    Add 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+1-filter_states.py
+
+This module contains a python script that lists all rows of
+'states' table whose 'name' starts with 'N' from 'hbtn_0e_0_usa'
+database.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_passwd,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'A%'"
+    result = cursor.execute(query)
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
