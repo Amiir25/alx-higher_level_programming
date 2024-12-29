a6584cfa6def2cc959f5762e3f07aed3259c7066
Amiir25 <amirsadik636@gmail.com>
2024-12-29 14:58:54 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,21 +7,26 @@ if __name__ == "__main__":
     mysql_passwd = sys.argv[2]
     mysql_db = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_user,
-        passwd=mysql_passwd,
-        db=mysql_db
-    )
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_passwd,
+            db=mysql_db
+        )
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
 
-    if 'cursor' in locals():
-        cursor.close()
-    if 'db' in locals():
-        db.close()
+    except Exception as e:
+        print(f"Error {e}")
+
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'db' in locals():
+            db.close()
