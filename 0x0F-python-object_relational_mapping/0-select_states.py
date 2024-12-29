d9de30bb19fbf192b0deb2cb952134f936166fe6
Amiir25 <amirsadik636@gmail.com>
2024-12-29 14:53:28 +0300

    Add 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_user,
+        passwd=mysql_passwd,
+        db=mysql_db
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    if 'cursor' in locals():
+        cursor.close()
+    if 'db' in locals():
+        db.close()
