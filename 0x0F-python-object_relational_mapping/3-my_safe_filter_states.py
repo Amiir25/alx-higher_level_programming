6ddf8b7f50dacc7cfaab4563bacb81005f706390
Amiir25 <amirsadik636@gmail.com>
2024-12-30 07:42:44 +0300

    Add 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+3-my_safe_filter_states.py
+
+This module contains a python script that lists the rows of
+'states' table whose 'name' maches the argument from 'hbtn_0e_0_usa'
+database, safe from MySQL injection.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_passwd,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+    state_name_escaped = MySQLdb.escape_string(state_name).decode()
+    query = (
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    ).format(state_name_escaped)
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    if 'cursor' in locals():
+        cursor.close()
+    if 'db' in locals():
+        db.close()
