5efe83f62470dc6fdb9367957fb746986653308d
Amiir25 <amirsadik636@gmail.com>
2025-01-16 16:46:27 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/bash
 # Takes a URL, sends a request to that URL and displays the size of the body
-
-# Check if URL is provided
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Assign the first argument as the URL
-URL="$1"
-
-# Send a request and calculate the size of the body
-SIZE=$(curl -s "$URL" -o /dev/null -w '%{size_download}')
-
-# Display the size
-echo "$SIZE"
+curl -s "$1" -o /dev/null -w '%{size_download}\n'
