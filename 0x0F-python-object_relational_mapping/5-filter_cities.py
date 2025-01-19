ab28d00b96eff8318b29d96f3f89c7c4ddead837
Amiir25 <amirsadik636@gmail.com>
2025-01-19 17:09:02 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,30 +17,35 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = pymysql.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_passwd,
-        db=database_name
-    )
+    try:
+        db = pymysql.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_passwd,
+            db=database_name
+        )
 
-    cursor = db.cursor()
-    query = (
-        "SELECT cities.name "
-        "FROM cities "
-        "JOIN states "
-        "ON cities.state_id=state.id"
-        "WHERE states.name = %s "
-        "ORDER BY cities.id"
-    )
-    cursor.execute(query, (state_name,))
+        cursor = db.cursor()
+        query = (
+            "SELECT cities.name "
+            "FROM cities "
+            "JOIN states "
+            "ON cities.state_id=state.id"
+            "WHERE states.name = %s "
+            "ORDER BY cities.id"
+        )
+        cursor.execute(query, (state_name,))
 
-    rows = cursor.fetchall()
-    cities = [row[0] for row in rows]
-    print(", ".join(cities))
+        rows = cursor.fetchall()
+        cities = [row[0] for row in rows]
+        print(", ".join(cities))
 
-    if 'cursor' in locals():
-        cursor.close()
-    if 'db' in locals():
-        db.close()
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'db' in locals():
+            db.close()
