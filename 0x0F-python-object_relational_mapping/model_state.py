bd43e77dfa8754011375adaf1b6bc0d0773309dc
Amir <amirsadik636@gmail.com>
2025-01-31 18:27:46 +0300

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,23 +11,35 @@ from sqlalchemy.ext.declarative import declarative_base
 # Create an instance of the declarative base
 Base = declarative_base()
 
+
 class State(Base):
     """State class that links to the MySQL table states."""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(
+            Integer, primary_key=True, autoincrement=True, nullable=False
+        )
     name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     from sys import argv
     if len(argv) != 4:
-        print("Usage: ./6-model_state.py <mysql username> <mysql password> <database name>")
+        print(
+            "Usage: ./6-model_state.py <mysql username>"
+            "<mysql password> <database name>"
+        )
+
     else:
         username = argv[1]
         password = argv[2]
         db_name = argv[3]
-        
+
         # Connect to the MySQL server
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}', pool_pre_ping=True)
-        
+        engine = create_engine(
+                f'mysql+mysqldb://{username}:{password}@'
+                'localhost:3306/{db_name}',
+                pool_pre_ping=True
+            )
+
         # Create all tables in the database
         Base.metadata.create_all(engine)
