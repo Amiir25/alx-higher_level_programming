609d1f08c5261f3ea4c7ad791f2d35b1886c37a2
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-01-31 18:21:53 +0300

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,32 +5,29 @@ This module contains a class definition of 'State' that inherits
 'Base' and is linked to the MySQL table 'states'.
 """
 
-import sys
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declaration import declaration_base
+from sqlalchemy.ext.declarative import declarative_base
 
+# Create an instance of the declarative base
 Base = declarative_base()
 
-
 class State(Base):
-    """
-    This class inherits from base and linked to MySQL table 'states'
-    contsaining 'id' and 'name' columns.
-    """
-
+    """State class that links to the MySQL table states."""
     __tablename__ = 'states'
-
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    connection_string = 'mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
-        usename, password, 3306, database
-    )
-    engine = create_engine(connection_string, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./6-model_state.py <mysql username> <mysql password> <database name>")
+    else:
+        username = argv[1]
+        password = argv[2]
+        db_name = argv[3]
+        
+        # Connect to the MySQL server
+        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}', pool_pre_ping=True)
+        
+        # Create all tables in the database
+        Base.metadata.create_all(engine)
