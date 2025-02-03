2d0a47ec75345258745ac48f7b1ce44c9baab29a
Amir <amirsadik636@gmail.com>
2025-02-03 15:35:07 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o /dev/null -w "%{http_code}" "http://example.com" | grep -q 200 && curl -s "http://example.com"
+curl -s -o /dev/null -w "%{http_code}" -L "$1" | grep -q 200 && curl -s -L "$1"
