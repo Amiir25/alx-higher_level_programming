f209a6cb81570a7bf1a7146d5222bd815b338ee2
Amir <amirsadik636@gmail.com>
2025-02-03 19:51:34 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond 'You got me!'.
-curl -sX GET 0.0.0.0:5000/catch_me
+curl -sX GET "http:// 0.0.0.0:5000/catch_me"
