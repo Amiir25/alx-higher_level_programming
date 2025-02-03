545ceb4c38e3491697cb8eb66af474abf977faa6
Amir <amirsadik636@gmail.com>
2025-02-03 17:52:11 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,22 @@
 '''
 6-peak.py
 
-This module finds a peak from an unsorted array
+This module finds a peak from a list unsorted integers
 '''
 
-def find_peak(arr):
+
+def find_peak(list_of_integers):
+    '''Finds a peak from a list of integers'''
+
+    if not list_of_integers:
+        return None
+
+    arr = list_of_integers
     left, right = 0, len(arr) - 1
 
     while left < right:
         mid = (left + right) // 2
-        
+
         if arr[mid] < arr[mid + 1]:  # Peak must be on the right
             left = mid + 1
         else:  # Peak is on the left or at mid
