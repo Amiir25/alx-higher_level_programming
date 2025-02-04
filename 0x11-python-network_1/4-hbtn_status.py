84811afb66587f3eacb4b804f4ba4edd6fb5a6a4
Amiir25 <amirsadik636@gmail.com>
2025-02-04 18:30:36 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,7 +11,7 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    body = response.content.decode('utf-8')
+    body = response.text
 
     print("Body response:")
     print("\t- type: ", type(body))
