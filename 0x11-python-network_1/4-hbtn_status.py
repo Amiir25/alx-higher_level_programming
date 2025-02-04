932fa876ac6fbf0c29124e31ebcffad2a65c884b
Amiir25 <amirsadik636@gmail.com>
2025-02-04 19:11:07 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     response = requests.get(url)
 
     print("Body response:")
-    print("\t- type: ", type(response.text))
-    print("\t- content: ", response.text)
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
