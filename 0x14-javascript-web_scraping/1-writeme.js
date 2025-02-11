9dc5689f7058d3a67f9806c242b4cefb0aed134a
Amiir25 <amirsadik636@gmail.com>
2025-02-11 18:35:09 +0300

    Add 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+if (!file || !content) {
+  console.error('Usage: ./1-writeme.js <file_path> <String>');
+  process.exit(1);
+}
+
+fs.writeFile(file, content, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
