6f4b7f64b5c8fc73dc85da587e52ea36a889f419
Amiir25 <amirsadik636@gmail.com>
2025-02-11 19:14:49 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
 
-if (!file || !content) {
+if (!file || !content || typeof content !== 'string') {
   console.error('Usage: ./1-writeme.js <file_path> <String>');
   process.exit(1);
 }
