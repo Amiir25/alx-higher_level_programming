641f36d070a5f81f79e97e04c59aca1655ab299a
Amiir25 <amirsadik636@gmail.com>
2025-02-11 18:19:35 +0300

    Add 0-readme.j

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+
+if (!file) {
+  console.error('Usage: ./0-readme.js <file_path>');
+  process.exit(1);
+}
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
