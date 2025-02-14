deb75b4f02128e5ffbb4806b6ae502c2d0382bb7
Amiir25 <amirsadik636@gmail.com>
2025-02-14 11:08:24 +0300

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,15 +1,4 @@
 -- A query that prints the full description of the
 -- first_table.
-SELECT
-    COLUMN NAME AS `Column`,
-    DATA_TYPE AS `Data Type`,
-    CHARACTER_MAXIMUM_LENGTH AS `Max Length`,
-    IS_NULLABLE AS `Nullable`,
-    COLUMN_DEFAULT AS `Default value`,
-    COLUMN_KEY AS `Key`,
-    EXTRA AS `Extra`
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHER
-    TABLE_SCHEMA = 'hbtn_0c_0'
-    AND TABLE_NAME = 'first_table';
+
+SHOW COLUMNS FROM first_table;
